a4ef0f3c5efcccc3384aea2c3bea1262c64f0892
jrodoni <55799673+jrodoni@users.noreply.github.com>
2022-05-10 15:45:32 -0700

    Add files via upload

---
+++ STAT 642 - Methods of STAT II (Spring 2022)/Course Notes/Week 7 (0227-0305)/RepSizeRandEffectFixedt_Findr.R
@@ -0,0 +1,23 @@
+
+
+
+repfixt <- function(alpha, gamma, t, sigma_a, sigma_e)
+{   r     <- 1
+    power <- 0
+    nu1   <- t-1
+    while(power < gamma) {
+        r      <- r+1
+        nu2    <- t*(r-1)
+        tau    <- (sigma_a/sigma_e)^2
+        lambda <- sqrt(1+r*tau)
+        Fcr    <- qf(1-alpha, nu1, nu2)
+        C      <- (1/lambda^2)*Fcr
+        power  <- 1-pf(C, nu1, nu2)
+    }
+    print(cbind(t, r, nu1, nu2, Fcr, lambda, power)) }
+
+repfixt(.01,.9,8,sqrt(2),1)
+
+
+r = seq(2,8,1)
+
